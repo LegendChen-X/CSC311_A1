ab025e300a1e7b5c6631d2554fcc487b44610be3
Xiang <chenxiang00825@gmail.com>
2020-09-19 21:44:47 -0400

    model built

--- hw1_q1_code.py
+++ hw1_q1_code.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 def load_data(clean_real_path,clean_fake_path):
     real_news_file = open(clean_real_path,'r')
@@ -17,13 +19,56 @@ def load_data(clean_real_path,clean_fake_path):
     
     vectorizer = CountVectorizer()
     X = vectorizer.fit_transform(all_news).toarray()
+    
     X_feature = vectorizer.get_feature_names()
     
-    label = np.vstack((np.zeros((fake_news_num, 1)), np.ones((real_news_num, 1))))
+    real_labels = np.ones((real_news_num,1))
+    fake_labels = np.zeros((fake_news_num,1))
     
-    X_train, X_test, Y_train, Y_test = train_test_split(X,train_target,test_size=0.7)
+    labels = np.concatenate((real_labels, fake_labels))
+    
+    X_train, X_test, y_train, y_test  = train_test_split(X,labels,test_size=0.7)
+    X_test, X_valid, y_test, y_valid = train_test_split(X_test,y_test,test_size=0.5)
+    
+    return {"X_train": X_train, "X_valid": X_valid, "X_test": X_test, "y_train": y_train, "y_valid": y_valid, "y_test": y_test, "X_feature": X_feature}
+    
+    
+def select_knn_model(clean_real_path,clean_fake_path):
+    result = {}
+    data_set = load_data(clean_real_path,clean_fake_path)
+    #Get data
+    X_train = data_set["X_train"]
+    X_valid = data_set["X_valid"]
+    #Get label
+    y_train = data_set["y_train"]
+    y_valid = data_set["y_valid"]
+    
+    k_set = [(i+1) for i in range(20)]
+    for value in k_set:
+        model = KNeighborsClassifier(n_neighbors = value)
+        model.fit(X_train,y_train.ravel())
+        train_predicted = model.predict(X_train)
+        valid_predicted = model.predict(X_valid)
+        
+        train_count = 0
+        for i in range(len(train_predicted)):
+            if train_predicted[i] != y_train[i]:
+                train_count += 1
+        result[value]=[train_count/len(train_predicted)]
+        
+        valid_count = 0
+        for i in range(len(valid_predicted)):
+            if valid_predicted[i] != y_valid[i]:
+                valid_count += 1
+                
+        result[value].append(valid_count/len(valid_predicted))
+        
+    return result
+                
+        
+print(select_knn_model("clean_real.txt","clean_fake.txt"))
     
     
     
-load_data("clean_real.txt","clean_fake.txt")
+    
     
