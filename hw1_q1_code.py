d6160ee4253412ea88b81403e4aef2b804779737
Xiang <chenxiang00825@gmail.com>
2020-09-29 22:55:03 -0400

    done

--- hw1_q1_code.py
+++ hw1_q1_code.py
@@ -7,34 +7,33 @@ from sklearn.neighbors import KNeighborsClassifier
 
 ###########------functions------###########
 def load_data(clean_real_path: str,clean_fake_path: str):
+# Get the file pointer
     real_news_file = open(clean_real_path,'r')
-    fake_nes_file = open(clean_fake_path,'r')
-    
+    fake_news_file = open(clean_fake_path,'r')
+# Load news into list
     real_news = [line.rstrip('\n') for line in real_news_file]
-    fake_news = [line.rstrip('\n') for line in fake_nes_file]
-    
+    fake_news = [line.rstrip('\n') for line in fake_news_file]
+# Combine real & fake news
     all_news = list(real_news+fake_news)
-    
+# Get the total length
     real_news_num = len(real_news)
     fake_news_num = len(fake_news)
-    
+
     total_num = real_news_num + fake_news_num
-    
+# Get Cv
     vectorizer = CountVectorizer()
     X = vectorizer.fit_transform(all_news)
-    
-    X_feature = vectorizer.get_feature_names()
-    
+# Make labels
     real_labels = np.ones((real_news_num,1))
     fake_labels = np.zeros((fake_news_num,1))
     
     labels = np.concatenate((real_labels, fake_labels))
+# Split them 7:3
+    X_train, X_test, y_train, y_test  = train_test_split(X,labels,train_size=0.7)
+# Split them 5 : 5 (Total is 0.3, both of them 0.15)
+    X_test, X_valid, y_test, y_valid = train_test_split(X_test,y_test,train_size=0.5)
     
-    X_train, X_test, y_train, y_test  = train_test_split(X,labels,test_size=0.7)
-    X_test, X_valid, y_test, y_valid = train_test_split(X_test,y_test,test_size=0.5)
-    
-    return {"X_train": X_train, "X_valid": X_valid, "X_test": X_test, "y_train": y_train, "y_valid": y_valid, "y_test": y_test, "X_feature": X_feature}
-    
+    return {"X_train": X_train, "X_valid": X_valid, "X_test": X_test, "y_train": y_train, "y_valid": y_valid, "y_test": y_test}
     
 def select_knn_model(clean_real_path: str,clean_fake_path: str,improvement: bool):
     result = {}
@@ -60,13 +59,13 @@ def select_knn_model(clean_real_path: str,clean_fake_path: str,improvement: bool
         model.fit(X_train,y_train.ravel())
         train_predicted = model.predict(X_train)
         valid_predicted = model.predict(X_valid)
-# Check train set
+# Check train set & get accuracy
         train_count = 0
         for i in range(len(train_predicted)):
             if train_predicted[i] == y_train[i]:
                 train_count += 1
         result[value]=[train_count/len(train_predicted)]
-# Check validate set & get
+# Check validate set & get accuracy
         valid_count = 0
         for i in range(len(valid_predicted)):
             if valid_predicted[i] == y_valid[i]:
@@ -80,9 +79,9 @@ def select_knn_model(clean_real_path: str,clean_fake_path: str,improvement: bool
             
 # Check test set with best-performed k
     if improvement:
-        model = KNeighborsClassifier(n_neighbors = value,metric = 'cosine')
+        model = KNeighborsClassifier(n_neighbors = best_k,metric = 'cosine')
     else:
-        model = KNeighborsClassifier(n_neighbors = value)
+        model = KNeighborsClassifier(n_neighbors = best_k)
     model.fit(X_train,y_train.ravel())
     test_predicted = model.predict(X_test)
     test_count = 0
@@ -91,9 +90,10 @@ def select_knn_model(clean_real_path: str,clean_fake_path: str,improvement: bool
             test_count += 1
             
     result[0]=[test_count/len(test_predicted)]
+    print("Best performance K is",best_k)
+    print("Perfoemance with best K is",result[0])
     return result
     
-
 def draw_graph(res,title):
     train_set = [res[item][0] for item in range(1,21)]
     validation_set = [res[item][-1] for item in range(1,21)]
@@ -112,5 +112,7 @@ def draw_graph(res,title):
 if __name__ == '__main__':
     res = select_knn_model("clean_real.txt","clean_fake.txt",False)
     draw_graph(res,"Original Graph")
+    for i in range(1,21):
+        print("When k is",i,", the training accuracy is",res[i][0],", the validation accuracy is",res[i][1])
     improve_res = select_knn_model("clean_real.txt","clean_fake.txt",True)
     draw_graph(improve_res,"Improvement Graph")
--- hw1_q4_code.py
+++ hw1_q4_code.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 def shuffle_data(data):
     t = data['t']
     X = data['X']
+# Get the list with random index
     random_permutation = np.random.permutation(len(X))
     res = {'X':[],'t':[]}
     for value in random_permutation:
@@ -14,10 +15,13 @@ def shuffle_data(data):
 def split_data(data,num_folds,fold):
     t = data['t']
     X = np.array(data['X'])
+# Get the length for each block
     block_length = len(t) // num_folds
     start_point = block_length * (fold - 1)
     end_point = block_length * fold
+# Hashmap for data fold
     data_fold = {'X':[],'t':[]}
+# Hashmap for data rest
     data_rest = {'X':[],'t':[]}
     data_fold['X'] = X[start_point:end_point, :]
     data_fold['t'] = t[start_point:end_point]
@@ -30,6 +34,7 @@ def train_model(data, lambd):
     X = data['X']
     X_norm = np.dot(X.T, X)
     N = len(X[0])
+# The formula from Q2
     invert_matrix = np.linalg.inv(X_norm + lambd * N * np.identity(N))
     res = np.dot(invert_matrix, np.dot(X.T,t))
     return res
@@ -44,6 +49,7 @@ def loss(data,model):
 
 def cross_validation(data, num_folds, lambd_seq):
     cv_error = np.zeros(len(lambd_seq))
+# Get data
     data = shuffle_data(data)
     for i in range(len(lambd_seq)):
         lambd = lambd_seq[i]
@@ -59,6 +65,7 @@ def train_and_test_error(data_train,data_test,lambd_seq):
     train_error = []
     test_error = []
     for i in lambd_seq:
+# Get model
         model = train_model(data_train,i)
         train_error.append(loss(data_train,model))
         test_error.append(loss(data_test,model))
@@ -75,21 +82,31 @@ def draw_graph(train_error,test_error,cv_error_5,cv_error_10,lambd_seq):
     plt.legend()
     plt.show()
     
-
 ###########------main------###########
 if __name__ == '__main__':
+# Get data from file
     data_train = {'X': np.genfromtxt('data_train_X.csv',delimiter=','),'t': np.genfromtxt('data_train_y.csv',delimiter=',')}
     data_test = {'X': np.genfromtxt('data_test_X.csv',delimiter=','),'t': np.genfromtxt('data_test_y.csv',delimiter=',')}
     lambd_seq = []
     interval = (0.005 - 0.00005) / 49
+# Get lambd_seq
     for i in range(50):
         lambd_seq.append(0.00005 + interval * i)
-        
+# Get train & test error
     train_error, test_error = train_and_test_error(data_train,data_test,lambd_seq)
-    print(train_error)
-    print(test_error)
+    
+    print("The error analysis for training & testing is")
+    for i in range(len(lambd_seq)):
+        print("When lambd is",lambd_seq[i],", training error is",train_error[i],", testing error is",test_error[i])
     
     cv_error_5 = cross_validation(data_train, 5, lambd_seq)
     cv_error_10 = cross_validation(data_train, 10, lambd_seq)
-    draw_graph (train_error, test_error, cv_error_5, cv_error_10,lambd_seq)
     
+    print("The error analysis for 5_folds is")
+    for i in range(len(lambd_seq)):
+        print("When lambd is",lambd_seq[i],", 5_folds error",cv_error_5[i])
+    print("The error analysis for 10_folds is")
+    for i in range(len(lambd_seq)):
+        print("When lambd is",lambd_seq[i],", 10_folds error",cv_error_10[i])
+# Draw the graph
+    draw_graph (train_error, test_error, cv_error_5, cv_error_10,lambd_seq)
